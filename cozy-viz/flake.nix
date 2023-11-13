2ce73d3d753a9e3cbcf0d8c351d76d3828cbb2f9
Graham <gleachkr@gmail.com>
2023-11-13 10:56:34 -0500

    Scope auxiluary derivations to where they are used.

--- cozy-viz/flake.nix
+++ cozy-viz/flake.nix
@@ -25,43 +25,52 @@
         dontNpmBuild = true;
       };
 
-      serve = let
+      defaultApp = let
         name = "serve-hungr-viz";
         script = pkgs.writeShellScriptBin name ''
           browser-sync start --listen 0.0.0.0 --server --index index.html --files="."
-          '';
+        '';
         buildInputs = [ 
           pkgs.nodePackages.browser-sync
           pkgs.nodejs
         ]; 
-      in pkgs.symlinkJoin {
-        name = name;
-        paths = [script] ++ buildInputs;
-        buildInputs = [ pkgs.makeWrapper ];
-        postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        serve = pkgs.symlinkJoin {
+          name = name;
+          paths = [script] ++ buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        }; 
+      in {
+        type = "app";
+        program = "${serve}/bin/serve-hungr-viz";
       };
 
-      bundle = let
+      apps.bundle = let
+        # this is derivation that sets up an environment to in which we can run
+        # the included bundler.mjs, which needs esbuild's JS modules
+        bundler = pkgs.buildNpmPackage {
+          name = "cozy-viz-bundler";
+          version = "0.0";
+          src = ./.;
+          npmDepsHash = "sha256-3DRXImi5vMZTJnLHJ3dtvQmeTDhqiFFlByudwvhYOV0=";
+          dontNpmBuild = true;
+        };
+
+        # this is a shell script for running bundler.mjs in that environment
         name = "bundle-cozy-viz";
         script = pkgs.writeShellScriptBin name ''
-          node ${self.packages.${system}.bundler}/lib/node_modules/cozy-viz/bundler.mjs
+          node ${bundler}/lib/node_modules/cozy-viz/bundler.mjs
         '';
-        buildInputs = [ self.packages.${system}.bundler ]; 
-      in pkgs.symlinkJoin {
-        name = name;
-        paths = [script] ++ buildInputs;
-        buildInputs = [ pkgs.makeWrapper ];
-        postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
-      };
-
-      defaultApp = {
+        buildInputs = [ bundler ]; 
+        bundle = pkgs.symlinkJoin {
+          name = name;
+          paths = [script] ++ buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+        };
+      in {
         type = "app";
-        program = "${self.serve.${system}}/bin/serve-hungr-viz";
-      };
-
-      apps.bundle = {
-        type = "app";
-        program = "${self.bundle.${system}}/bin/bundle-cozy-viz";
+        program = "${bundle}/bin/bundle-cozy-viz";
       };
   });
 }
