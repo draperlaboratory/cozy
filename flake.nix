0d653c4f525b0d6bc8b9999a8f99939566af07dd
Graham <gleachkr@gmail.com>
2023-11-28 15:34:23 -0500

    Add portion to nix flakes

--- flake.nix
+++ flake.nix
@@ -17,13 +17,24 @@
       ];
     };
 
-    devShells.x86_64-linux.testing = pkgs.mkShell {
+    devShells.x86_64-linux.testing = let 
+      portion = pyPkgs.buildPythonPackage rec {
+        pname = "portion";
+        version = "2.4.1";
+        src = pyPkgs.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-ncvxgIiY9ECu0wSl6fB0KihZ7KOwrH8fWOUFAoUqjvk=";
+        };
+        doCheck = false;
+      };
+    in pkgs.mkShell {
       buildInputs = [ 
         pkgs.python311
         pkgs.python311Packages.angr
         pkgs.python311Packages.networkx
         pkgs.python311Packages.sphinx
         pkgs.python311Packages.sphinx-autoapi
+        portion
         self.packages.x86_64-linux.default
       ];
     };
