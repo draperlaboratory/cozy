3d461db68291f0a23609811f15182f851f7bc539
Graham <gleachkr@gmail.com>
2023-12-29 17:14:33 -0500

    add simple_cond example

---
+++ examples/cmp_simple_cond.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from cozy.project import Project
+from cozy.analysis import Comparison
+import cozy.execution_graph as execution_graph
+
+import claripy
+
+proj_pre = Project('test_programs/simple_branch/simpleBranch-pre')
+
+proj_post = Project('test_programs/simple_branch/simpleBranch-post')
+
+sess_pre = proj_pre.session("main")
+
+sess_post = proj_post.session("main")
+
+arg = claripy.BVS('n', 4 * 8)
+
+rslt_pre = sess_pre.run(arg, cache_intermediate_states=True)
+
+rslt_post = sess_post.run(arg, cache_intermediate_states=True)
+
+comparison = Comparison(rslt_pre, rslt_post)
+
+execution_graph.dump_comparison(proj_pre, proj_post, rslt_pre, rslt_post, comparison, "simple_pre.json","simple_post.json", args=[arg], include_actions=True)
