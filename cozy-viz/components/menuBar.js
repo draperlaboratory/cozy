e3f9f3382d8d1d46fe7e381bbd96cbf89f6cb25c
Graham <gleachkr@gmail.com>
2024-03-01 14:55:50 -0500

    Factor out Search Menu component

--- cozy-viz/components/menuBar.js
+++ cozy-viz/components/menuBar.js
@@ -156,7 +156,6 @@ export default class MenuBar extends Component {
     }
   }
 
-
   setTidiness(level) {
     this.props.setTidiness(level)
     this.setOpen(null)
@@ -165,36 +164,6 @@ export default class MenuBar extends Component {
   resetLayout() {
     this.props.resetLayout()
     this.setOpen(null)
-  }
-
-  updateSearch(e) {
-    this.setState({ searchStdoutRegex: e.target.value }, () => {
-      const cyLeft = this.props.cyLeft.cy
-      const cyRight = this.props.cyRight.cy
-      cyLeft.dim()
-      cyRight.dim()
-      let regex
-      try {
-        regex = new RegExp(this.state.searchStdoutRegex)
-      } catch (e) {
-        return
-      }
-      const ltargets = cyLeft.nodes()
-        .filter(node => node.data().stdout.match(regex))
-      const rtargets = cyRight.nodes()
-        .filter(node => node.data().stdout.match(regex))
-      cyLeft.highlight(ltargets)
-      cyRight.highlight(rtargets)
-    })
-  }
-
-  clearSearch() {
-    this.setState({ searchStdoutRegex: '.*' }, () => {
-      const cyLeft = this.props.cyLeft.cy
-      const cyRight = this.props.cyRight.cy
-      cyLeft.dim()
-      cyRight.dim()
-    })
   }
 
   saveFile(data) {
@@ -284,12 +253,62 @@ export default class MenuBar extends Component {
             Reset
         <//>
       <//>
-      <${Menu} 
+      <${SearchMenu}
         enabled=${enabled}
         open=${state.open}
+        setOpen=${o => this.setOpen(o)}
+        cyLeft=${props.cyLeft}
+        cyRight=${props.cyRight}
+      />
+    </div>`
+  }
+}
+
+class SearchMenu extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchStdoutRegex: ".*",
+    }
+  }
+
+  updateSearch(e) {
+    this.setState({ searchStdoutRegex: e.target.value }, () => {
+      const cyLeft = this.props.cyLeft.cy
+      const cyRight = this.props.cyRight.cy
+      cyLeft.dim()
+      cyRight.dim()
+      let regex
+      try {
+        regex = new RegExp(this.state.searchStdoutRegex)
+      } catch (e) {
+        return
+      }
+      const ltargets = cyLeft.nodes()
+        .filter(node => node.data().stdout.match(regex))
+      const rtargets = cyRight.nodes()
+        .filter(node => node.data().stdout.match(regex))
+      cyLeft.highlight(ltargets)
+      cyRight.highlight(rtargets)
+    })
+  }
+
+  clearSearch() {
+    this.setState({ searchStdoutRegex: '.*' }, () => {
+      const cyLeft = this.props.cyLeft.cy
+      const cyRight = this.props.cyRight.cy
+      cyLeft.dim()
+      cyRight.dim()
+    })
+  }
+
+  render(props, state) {
+    return html`<${Menu} 
+        enabled=${props.enabled}
+        open=${props.open}
         title="Search"
-        setOpen=${o => this.setOpen(o)}>
-        <${MenuOption} onClick=${() => this.setOpen(null)}>
+        setOpen=${o => props.setOpen(o)}>
+        <${MenuOption} onClick=${() => props.setOpen(null)}>
           Stdout <input 
             onClick=${e => e.stopPropagation()}
             onInput=${e => this.updateSearch(e)} 
@@ -298,8 +317,7 @@ export default class MenuBar extends Component {
         <${MenuOption} onClick=${() => this.clearSearch(null)}>
           Clear Search 
         <//>
-      <//>
-    </div>`
+      <//>`
   }
 }
 
