466302940c2f615879fdd8e567113651731d9181
Caleb Helbling <caleb.helbling@yahoo.com>
2024-03-06 12:59:13 -0500

    Updated grididps demo

--- examples/grididps.py
+++ examples/grididps.py
@@ -2,29 +2,32 @@ import cozy
 import claripy
 import angr
 
+from cozy import claripy_ext
+
 BUFFER_SIZE = 18
 
 proj_prepatched = cozy.project.Project('test_programs/GridIDPS/build/amp_challenge_arm.ino_unstripped.elf')
+proj_goodpatch = cozy.project.Project('test_programs/GridIDPS/build/ids_bin_earlier_patch')
 
 buffer_position_sym = claripy.BVS('bufferPosition', 32)
-available_sym = claripy.BVS('SerialAvailable', 32)
-read_sym = claripy.BVS('ReadSym', 8)
+available_symbols = [claripy.BVS('SerialAvailable', 32) for i in range(7)]
+available_symbols.append(claripy.BVV(0, 32))
+read_symbols = [claripy.BVS('ReadSym', 8) for j in range(32)]
 inputBuffer_sym = [claripy.BVS('inputBuffer_sym_{}'.format(i), 8) for i in range(BUFFER_SIZE)]
 
 class usb_serial_available(angr.SimProcedure):
-    # pylint: disable=arguments-differ
     def run(self):
-        print('doing usb_serial_available')
+        available_sym = available_symbols[self.state.globals['available_i']]
+        self.state.globals['available_i'] += 1
         return available_sym
 
 class usb_serial_getchar(angr.SimProcedure):
-    # pylint: disable=arguments-differ
     def run(self):
-        print('doing usb_serial_getchar')
+        read_sym = read_symbols[self.state.globals['readsym_i']]
+        self.state.globals['readsym_i'] += 1
         return read_sym.zero_extend(32 - 8)
 
 class usb_serial_write(angr.SimProcedure):
-    # pylint: disable=arguments-differ
     def run(self, str, str_len):
         puts = angr.SIM_PROCEDURES["libc"]["puts"]
         self.inline_call(puts, str)
@@ -32,17 +35,32 @@ class usb_serial_write(angr.SimProcedure):
 
 class println(angr.SimProcedure):
     def run(self, arg):
-        print('doing println')
         return 0
+
+class setup_hook(angr.SimProcedure):
+    def run(self):
+        return None
+
+class yield_hook(angr.SimProcedure):
+    def run(self):
+        return None
 
 def run(proj: cozy.project.Project):
     proj.hook_symbol('usb_serial_available', usb_serial_available)
     proj.hook_symbol('usb_serial_getchar', usb_serial_getchar)
     proj.hook_symbol('usb_serial_write', usb_serial_write)
     proj.hook_symbol('_ZN5Print7printlnEv', println)
+    proj.hook_symbol('setup', setup_hook)
+    proj.hook_symbol('yield', yield_hook)
     proj.add_prototype('loop', 'void loop()')
+    proj.add_prototype('main', 'int main()')
 
     sess = proj.session('loop')
+
+    sess.add_constraints(claripy_ext.twos_comp_range_constraint(buffer_position_sym, 0, 128))
+
+    sess.state.globals['available_i'] = 0
+    sess.state.globals['readsym_i'] = 0
 
     command_log_addr = sess.malloc(3 * BUFFER_SIZE)
     class process_command(angr.SimProcedure):
@@ -77,19 +95,21 @@ def run(proj: cozy.project.Project):
     return sess.run(loop_arguments)
 
 results_prepatched = run(proj_prepatched)
+results_goodpatch = run(proj_goodpatch)
 
-comparison_results = cozy.analysis.Comparison(results_prepatched, results_prepatched)
+comparison_results = cozy.analysis.Comparison(results_prepatched, results_goodpatch)
 
 def concrete_arg_mapper(args):
     ret = dict(args)
     ret['bufferPosition'] = cozy.primitives.from_twos_comp(args['bufferPosition'].concrete_value, 32)
-    ret['available'] = False if args['available'].concrete_value == 0 else True
-    ret['read_sym'] = chr(args['read_sym'].concrete_value)
+    ret['available'] = [False if av.concrete_value == 0 else True for av in args['available']]
+    ret['read_sym'] = [chr(r.concrete_value) for r in args['read_sym']]
     return ret
 
-cozy.execution_graph.visualize_comparison(proj_prepatched, proj_prepatched,
-                                          results_prepatched, results_prepatched,
+cozy.execution_graph.visualize_comparison(proj_prepatched, proj_goodpatch,
+                                          results_prepatched, results_goodpatch,
                                           comparison_results,
+                                          include_actions=True,
                                           args={"bufferPosition": buffer_position_sym, "inputBuffer": inputBuffer_sym,
-                                                'read_sym': read_sym, 'available': available_sym},
+                                                'read_sym': read_symbols, 'available': available_symbols},
                                           num_examples=2, open_browser=True, concrete_arg_mapper=concrete_arg_mapper)
